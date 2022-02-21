759e1d78b4ad846dc02d2cc4c7c7f3222f6d8fed
kimhelp <operat8009@gmail.com>
2022-02-21 18:05:24 +0900

    로그인,회원가입 구현

---
+++ routers/index.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const router = express.Router()
+const user = require('../models/user')
+const {alertmove} = require('../util/alert')
+const userRouter = require('./user')
+const boardRouter = require('./board')
+
+router.get('/',(req,res)=>{
+    const {user} = req.session
+    res.render('index',{
+        user
+    })
+})
+
+const login = (req,res,next)=>{
+    let { user } = req.session
+        if (user != undefined) {
+            next()
+        } else {
+            res.send(alertmove('/','권한 없음'))
+        }
+    }
+
+router.use('/user',userRouter)
+router.use('/board',login,boardRouter)
+
+module.exports = router
--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express')
-const app = express()
+const nunjucks = require('nunjucks')
+const session = require('express-session')
+const Memorystore = require('memorystore')(session)
+const bodyParser = require('body-parser')
+const PORT = process.env.PORT || 3000
 
-app.get('/', (req ,res) => {
-  res.send('main')
+const app = express()
+const router = require('./routers')
+
+
+app.set('view engine','html')
+nunjucks.configure('views',{
+    express:app,
 })
 
+const maxAge = 6000
+let sessionObj = {
+    secret: "ingoo",
+    resave : false,
+    saveuninitialized: true,
+    store: new Memorystore({ checkPeriod: maxAge}),
+    cookie:{
+        maxAge:maxAge
+    }
+}
+
+app.use(session(sessionObj))
+app.use(express.urlencoded({extended:true,}))
+app.use(express.json())
+app.use(bodyParser.urlencoded({extended:true}))
+
+app.use(router)
+
 app.listen(3000, () => {
-  console.log('server start')
+  console.log(`server start : ${PORT}`)
 })
